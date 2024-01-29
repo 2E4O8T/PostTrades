12c00e3402ed10a0cfdda60b5600ee696eca9a4b
2E4O8T <benoit.froidevan@axa.fr>
2024-01-29 14:59:44 +0100

    Add-Database

---
+++ PostTrades/Data/PostTradesDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostTrades.Domain;
+
+namespace PostTrades.Data
+{
+    public class PostTradesDbContext :DbContext
+    {
+        public PostTradesDbContext(DbContextOptions<PostTradesDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Bid> Bid { get; set; }
+        public DbSet<CurvePoint> CurvePoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RuleName> RuleNames { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Bid>();
+            modelBuilder.Entity<CurvePoint>();
+            modelBuilder.Entity<Rating>();
+            modelBuilder.Entity<RuleName>();
+            modelBuilder.Entity<Trade>();
+        }
+    }
+}
--- PostTrades/Program.cs
+++ PostTrades/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PostTrades.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add database connection
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+
+builder.Services.AddDbContext<PostTradesDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
